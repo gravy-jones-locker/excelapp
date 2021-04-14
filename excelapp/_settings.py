09d2b972dca931f7833926cac102e7b155a65558
gravy johnson <gravy@gravyjoneslock.uk>
2021-04-14 09:34:38 +0100

    Updated ip info

--- excelapp/_settings.py
+++ excelapp/_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#1vh8kf@-iaha0k(ndqvqe)8007jnk6bl2fyjq!&r9j9b^3(a%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
